124401bb5b07b6682b2fe0c6a8098145794a003e
alfonset <alfonset@mit.edu>
2020-12-18 17:12:03 -0500

    Fixed util.jl

--- src/util.jl
+++ src/util.jl
@@ -2,7 +2,6 @@ function create_function_of_sobol(ps)
     """
     Create a function of sobol, parameterized by the the vector ps.
     The function of sobol is widely used as a sensitivity analysis benchmark.
-    https://www.sciencedirect.com/science/article/pii/S0951832005001444
     """
 
     g(x, p) = (abs(4*x-2)+p)/(1+p) # for 0 <= Xi <= 1 and ai >= 0
@@ -21,7 +20,7 @@ end
 
 function ishigami(X)
     """
-        Ishigami function. Takes a vector, returns a scalar.
+        Ishigami function. Takes a vector input, returns a scalar.
     """
     X1, X2, X3, X4 = X
     Y = sin(X1) + 7*sin(X2)^2 + 0.1*X3^4*sin(X1)
@@ -31,8 +30,9 @@ end
 
 function create_ishigami_samples(sample_size)
     """
-        Create sample input vectors for the ishigami function
-        and return 
+        Sample input vectors for the ishigami function,
+        evaluate the ishigami function on them,
+        and returns a vector of the scalar outputs.
     """
     X = rand(Uniform(-pi, pi), sample_size, 4)
     Y = zeros(sample_size)
